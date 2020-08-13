fff571f72a4c8307cc245f45c51fc16cfa55b0b4
Alex Lin <alexlin247@gmail.com>
2020-08-13 01:43:09 -0400

    Add parallax image component

---
+++ src/components/shared/parallax-image/parallax-image.tsx
@@ -0,0 +1,63 @@
+import React, { ReactNode, useRef, useEffect, useState } from 'react';
+import styles from './parallax.module.scss';
+
+type Props = {
+  color?: string;
+  url: string;
+  width?: string;
+  height?: string;
+  aspectRatio?: number;
+  speed?: number;
+  children?: ReactNode;
+  className?: string;
+};
+
+const ParallaxImage: React.FC<Props> = (props) => {
+  const elemRef = useRef(null);
+  let initialOffset;
+  const [imageHeight, setImageHeight] = useState(0);
+  const [offset, setOffset] = useState(0);
+  useEffect(() => {
+    setImageHeight(elemRef.current.offsetWidth * props.aspectRatio);
+    initialOffset =
+      (elemRef.current.getBoundingClientRect().top +
+        window.scrollY -
+        elemRef.current.offsetHeight -
+        offset) *
+      props.speed;
+    setOffset(initialOffset);
+    window.addEventListener('scroll', (e: Event) => {
+      setOffset(initialOffset - window.scrollY * props.speed);
+      console.log(initialOffset);
+    });
+  }, []);
+
+  return (
+    <div
+      className={`${styles.container} ${props.className}`}
+      style={{
+        width: props.width,
+        height: props.height,
+      }}
+      ref={elemRef}
+    >
+      <div
+        className={styles.parallax}
+        style={{
+          transform: `translateY(${offset}px)`,
+          height: imageHeight,
+          backgroundColor: props.color,
+          backgroundImage: `url(${props.url}`,
+        }}
+      ></div>
+      <div className="absolute w-full">{props.children}</div>
+    </div>
+  );
+};
+
+ParallaxImage.defaultProps = {
+  aspectRatio: 1,
+  speed: 0.2,
+};
+
+export default ParallaxImage;
