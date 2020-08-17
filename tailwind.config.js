16348452bcb7df838f1ea4a6df99aba4ec764eb8
Alex Lin <alexlin247@gmail.com>
2020-08-17 00:28:37 -0400

    Add custom theme colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,32 @@ module.exports = {
   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      colors: {
-        'accent-1': '#333',
-      },
+      colors: Object.assign(
+        {},
+        ...[...Array(9).keys()].map((i) => {
+          return {
+            [`primary-${(i + 1) * 100}`]: `var(--color-primary-${
+              (i + 1) * 100
+            })`,
+          };
+        }),
+        ...[...Array(9).keys()].map((i) => {
+          return {
+            [`secondary-${(i + 1) * 100}`]: `var(--color-secondary-${
+              (i + 1) * 100
+            })`,
+          };
+        }),
+        ...[...Array(9).keys()].map((i) => {
+          return {
+            [`accent-${(i + 1) * 100}`]: `var(--color-accent-${
+              (i + 1) * 100
+            })`,
+          };
+        })
+      ),
     },
   },
   variants: {},
   plugins: [],
-}
+};
