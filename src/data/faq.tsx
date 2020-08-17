bc5f0f27f4bb5ee33f4f0c23585dec05581e9987
Alex Lin <alexlin247@gmail.com>
2020-08-17 00:27:02 -0400

    Add FAQ data

---
+++ src/data/faq.tsx
@@ -0,0 +1,90 @@
+export default [
+  {
+    question: 'Where is it held?',
+    answer: (
+      <p>
+        Due to COVID-19 restrictions, all rounds of TECC 2021 will be held
+        completely online.
+      </p>
+    ),
+  },
+  {
+    question: 'What is the competition format?',
+    answer: (
+      <div>
+        <p>TECC will be comprised of two rounds:</p>
+        <ol className="list-disc pl-4">
+          <li>
+            All teams have a week to solve a real-life business case. Solutions
+            are uploaded through our online platform.
+          </li>
+          <li>
+            The teams that submit a satisfactory solution will continue to the
+            finals which is held online as a video conference with industry
+            professionals.
+          </li>
+        </ol>
+      </div>
+    ),
+  },
+  {
+    question: 'Who will be judging my work?',
+    answer: (
+      <p>
+        Preliminary entries will be judged by our team. Finalists will present
+        to industry professionals over a video conference.
+      </p>
+    ),
+  },
+  {
+    question: 'How do I form a team?',
+    answer: (
+      <p>
+        Teams have to consist of 4 eligible students, which can be formed across
+        schools or states. Find some other friends or classmates and sign-up!
+      </p>
+    ),
+  },
+  {
+    question: 'How do I sign up?',
+    answer: (
+      <p>
+        Simple, just click this form here [link]! Only one person from each team
+        needs to sign up to create a new team.
+      </p>
+    ),
+  },
+  {
+    question: 'Who can participate?',
+    answer: (
+      <p>TECC 2021 is open to 7th to 12th graders from across the country.</p>
+    ),
+  },
+  {
+    question: 'Are there prizes?',
+    answer: (
+      <p>
+        Yes! We have over <span className="text-green-500 font-bold">$1.0000</span> in prizes and they will be
+        disclosed closer to the actual competition!
+      </p>
+    ),
+  },
+  {
+    question: 'What does it cost?',
+    answer: (
+      <p>
+        Absolutely nothing! There is no cost for participating and all prizes
+        and shipping fees will be covered by TECC.
+      </p>
+    ),
+  },
+  {
+    question: 'Further questions?',
+    answer: (
+      <p>
+        If you have any further questions, please do not hesitate to contact us
+        at tj.tecc@gmail.com
+      </p>
+    ),
+  },
+];
