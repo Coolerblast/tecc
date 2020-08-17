07f7d284b5239ca4f7f26f60e31e9c27eb368e5a
Alex Lin <alexlin247@gmail.com>
2020-08-17 00:27:16 -0400

    Add timeline data

---
+++ src/data/timeline.tsx
@@ -0,0 +1,32 @@
+export default [
+  {
+    title: 'Registration Opens',
+    date: 'January 22nd, 2021',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, quia.',
+  },
+  {
+    title: 'Case Launch',
+    date: 'March 32nd, 2021',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, quia.',
+  },
+  {
+    title: 'Case Submission',
+    date: 'March 33rd, 2021',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, quia.',
+  },
+  {
+    title: 'Finalist Decisions',
+    date: 'April 5th, 2021',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, quia.',
+  },
+  {
+    title: 'Finalist Presentation',
+    date: 'April 10th, 2021',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, quia.',
+  },
+];
