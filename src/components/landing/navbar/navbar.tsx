921601a8e4cbec3e35e9c32289a3c41b3bd0d185
Alex Lin <alexlin247@gmail.com>
2020-08-17 21:46:52 -0400

    Make NavBar responsive

--- src/components/landing/navbar/navbar.tsx
+++ src/components/landing/navbar/navbar.tsx
@@ -1,27 +1,40 @@
 import Link from 'next/link';
 import styles from './navbar.module.scss';
+import { useState } from 'react';
 
 type Props = {
   links: { href: string; label: string }[];
 };
 
 const NavBar: React.FC<Props> = (props) => {
+  const [isCollapsed, setCollapsed] = useState(true);
   return (
-    <nav>
-      <ul className={styles.navbar}>
-        <li>
+    <nav className={styles.navbar}>
+      <div>
+        <div className={styles['brand-wrapper']}>
           <Link href="/">
             <a className={styles.brand}>tecc</a>
           </Link>
-        </li>
-        <ul className={`${styles.links} space-x-4`}>
-          {props.links.map(({ href, label }) => (
-            <li key={`${href}${label}`}>
-              <a href={href}>{label}</a>
-            </li>
-          ))}
-        </ul>
-      </ul>
+          <div className={styles.button} onClick={() => setCollapsed(!isCollapsed)}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+        <div
+          className={`${styles['links-wrapper']} ${
+            isCollapsed ? styles.collapse : ''
+          }`.trim()}
+        >
+          <ul className={styles.links}>
+            {props.links.map(({ href, label }) => (
+              <li key={`${href}${label}`}>
+                <a href={href}>{label}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
