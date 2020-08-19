b0c923a5ffdade9fd5666e541b416ffb3f36b18c
Alex Lin <alexlin247@gmail.com>
2020-08-19 16:56:13 -0400

    Style quote section

--- src/components/landing/sections/quote/quote.tsx
+++ src/components/landing/sections/quote/quote.tsx
@@ -4,15 +4,21 @@ import styles from './quote.module.scss';
 
 const Quote = () => {
   return (
-    <div>
-      <p className="text-xl sm:text-2xl md:text-3xl text-gray-800 p-8 leading-relaxed">
-        <strong>Technology is rapidly changing our lives</strong>, and the laws
-        surrounding them aren’t keeping up. Businesses are therefore faced with
-        decisions between profit and morals. Come to <strong>TECC</strong> to
-        stun industry professionals with your scholarship, enhance your
-        decision-making skills, and participate in an unique opportunity to
-        share ideas with peers who have diverse backgrounds and perspectives.
-      </p>
+    <div className={styles.container}>
+      <div>
+        
+      </div>
+      <div>
+        <p>
+          <strong>Technology is rapidly changing our lives</strong>, and the
+          laws surrounding them aren’t keeping up. Businesses are therefore
+          faced with decisions between profit and morals. Come to{' '}
+          <strong>TECC</strong> to stun industry professionals with your
+          scholarship, enhance your decision-making skills, and participate in
+          an unique opportunity to share ideas with peers who have diverse
+          backgrounds and perspectives.
+        </p>
+      </div>
     </div>
   );
 };
