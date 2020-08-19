32919a3f1d91c19e60fac1f43eb5e30e5fba6b96
Alex Lin <alexlin247@gmail.com>
2020-08-19 16:56:53 -0400

    Style About section

--- src/components/landing/sections/about/about.tsx
+++ src/components/landing/sections/about/about.tsx
@@ -4,21 +4,29 @@ import styles from './about.module.scss';
 
 const About = () => {
   return (
-    <div className="py-32 w-3/4 mx-auto">
-      <section id="about">
-        <p className="text-2xl text-gray-800 p-8 leading-relaxed">
-          TECC is a <strong>one week</strong> long case competition with a final
-          online presentation in the presence of industry professionals. The
-          teams will have to perform the following tasks:
-        </p>
-        <ol className="list-disc pl-10">
-          <li>Ethical Analysis</li>
-          <li>Technological Analysis</li>
-          <li>Sociopolitical Decision-Making</li>
+    <div className={styles.container}>
+      <h1>
+        TECC is a <strong>one week</strong> long case competition with a final
+        online presentation in the presence of industry professionals. The teams
+        will have to perform the following tasks:
+      </h1>
+      <div>
+        <ol>
+          <li>
+            <p>Ethical Analysis</p>
+          </li>
+          <li>
+            <p>Technological Analysis</p>
+          </li>
+          <li>
+            <p>Sociopolitical Decision-Making</p>
+          </li>
+        </ol>
+        <h2>
           Students will work in a dynamic environment in which they will get
           first-hand experience on policy-making and cost-benefit analysis.
-        </ol>
-      </section>
+        </h2>
+      </div>
     </div>
   );
 };
