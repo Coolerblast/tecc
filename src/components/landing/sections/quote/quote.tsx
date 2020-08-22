82253a12150aceafec2f9cb125a83ab2f735c3e1
Alex Lin <alexlin247@gmail.com>
2020-08-21 20:36:16 -0400

    Make quote more responsive

--- src/components/landing/sections/quote/quote.tsx
+++ src/components/landing/sections/quote/quote.tsx
@@ -5,19 +5,19 @@ import styles from './quote.module.scss';
 const Quote = () => {
   return (
     <div className={styles.container}>
-      <div>
-        
-      </div>
-      <div>
-        <p>
-          <strong>Technology is rapidly changing our lives</strong>, and the
-          laws surrounding them aren’t keeping up. Businesses are therefore
-          faced with decisions between profit and morals. Come to{' '}
-          <strong>TECC</strong> to stun industry professionals with your
-          scholarship, enhance your decision-making skills, and participate in
-          an unique opportunity to share ideas with peers who have diverse
-          backgrounds and perspectives.
-        </p>
+      <div className={styles.wrapper}>
+        <div></div>
+        <div>
+          <p>
+            <strong>Technology is rapidly changing our lives</strong>, and the
+            laws surrounding them aren’t keeping up. Businesses are therefore
+            faced with decisions between profit and morals. Come to{' '}
+            <strong>TECC</strong> to stun industry professionals with your
+            scholarship, enhance your decision-making skills, and participate in
+            an unique opportunity to share ideas with peers who have diverse
+            backgrounds and perspectives.
+          </p>
+        </div>
       </div>
     </div>
   );
