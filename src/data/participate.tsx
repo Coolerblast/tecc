02e707533c7126771b19ab6bc4e3271726e6d3f4
Alex Lin <alexlin247@gmail.com>
2020-08-30 14:37:53 -0400

    Add participate info

---
+++ src/data/participate.tsx
@@ -0,0 +1,22 @@
+export default [
+  {
+    title: 'Stand Out',
+    description:
+      'Demonstrate that you have the skills to succeed in the real world. TECC 2021 will test your critical thinking, presentation technique and case-solving skills.',
+  },
+  {
+    title: 'Industry Experience',
+    description:
+      'Participating in case competitions is a great way for you to test your skills and knowledge from the classroom in a real-life business case challenge. ',
+  },
+  {
+    title: 'Challenge Your Stances',
+    description:
+      'Thinking through solutions allows you to discover where you stand in certain issues, and which ones should be prioritized, helping you shape your ethical subjectivities.',
+  },
+  {
+    title: 'Increase Awareness',
+    description:
+      'Discover the impact of technology in the information age. Participating in TECC will allow you to gain crucial information about the latest innovations that could impact your lives.',
+  },
+];
